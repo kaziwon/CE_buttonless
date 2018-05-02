2594ab10e838e74abaef9638725804b94fddbccf
João Cabral <jv_cabral@outlook.com>
2018-05-02 15:22:44 -0300

    sd

---
+++ popup.js
@@ -0,0 +1,15 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+$(document).ready(function(element){
+  
+   
+        $('img').each(function(){$(this).attr('src', 'https://marketingdeconteudo.com/wp-content/uploads/2017/01/formatos-de-imagem-2.jpg')});
+
+    });
+   
+      
+
